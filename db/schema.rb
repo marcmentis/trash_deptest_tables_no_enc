f7e6acd67084123a3ef0711ffc05a1b3048dc0d5
Marc Mentis <mmentis@optonline.net>
2015-03-29 16:20:19 -0400

    Add migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150328180427) do
+ActiveRecord::Schema.define(version: 20150328224335) do
 
   create_table "patients", force: true do |t|
     t.string   "firstname"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 20150328180427) do
     t.integer  "cnumber"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "col1"
+    t.string   "col2"
+    t.string   "col3"
+    t.string   "col4"
   end
 
 end
